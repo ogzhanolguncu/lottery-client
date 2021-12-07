410c9c585f1d1e42519e7ff9a9af38c11a132e82
Oguzhan Olguncu <oguzhan@fireflyon.com>
2021-12-07 16:42:16 +0300

    Add connec to wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import { Flex, Heading, Input, Box, Button, Text } from "@chakra-ui/react";
 
+import invariant from "invariant";
+
+const connectToMetamask = async () => {
+  try {
+    invariant(window.ethereum, "No crypto wallet found. Please install it.");
+    window.ethereum.request({ method: "eth_requestAccounts" });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
 const App = () => {
+  useEffect(() => {
+    connectToMetamask();
+  }, []);
+
   return (
     <Flex
       justifyContent="center"
@@ -19,12 +35,12 @@ const App = () => {
           placeholder="Deposit Amount"
           focusBorderColor="#916BBF"
           backgroundColor="transparent"
-          _hover={{ borderColor: "#916BBF" }}
           borderRadius="30px"
           border="2px solid"
           borderColor="#916BBF"
           color="#fff"
           _placeholder={{ color: "gray.100" }}
+          _hover={{ borderColor: "#916BBF" }}
         />
         <Flex width="100%" justifyContent="center">
           <Button
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
+import { MetaMaskInpageProvider } from "@metamask/providers";
 /// <reference types="react-scripts" />
+declare global {
+  interface Window {
+    ethereum: MetaMaskInpageProvider;
+  }
+}
